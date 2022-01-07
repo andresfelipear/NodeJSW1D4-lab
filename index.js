44e14741fd92328bcd0f21cf7a48677f946c185e
Andres Arevalo <andresfelipear@gmail.com>
2022-01-07 12:53:43 -0800

    page 404 done!

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const admin = require('./routes/admin')
+const path = require('path')
+const rootDirectory = require('./util/path')
 
 
 const app = express()
@@ -13,7 +15,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static('./public')) //serve files statically
 app.use(admin)
-
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(rootDirectory, 'views', '404.html'))
+})
 
 
 app.listen(8000)
